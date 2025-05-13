58abadf6478e71008b4e03576c369a5faae1efa2
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-05-13 19:37:02 +0700

    definisi NIM dan NAMA

--- constructoveloading.cpp
+++ constructoveloading.cpp
@@ -34,12 +34,19 @@ mahasiswa::mahasiswa(int iNim, string iNama){
 void mahasiswa::cetak(){
 
     cout << endl << "Nim = "<<nim << endl;
-    cout <<
-    
+    cout << endl << "Nama = "<<nama << endl;
 };
 
 int main (){
     mahasiswa mhs1(102030); 
     mahasiswa mhs2("Andi");
+    mahasiswa mhs3(012030, "Santi");
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+
 }
 
